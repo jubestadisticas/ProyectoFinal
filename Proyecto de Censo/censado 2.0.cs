852f6f32e7eeaa44d45c9353dd3c1110aa3276b7
Jofiel Ubri <jofielubri@gmail.com>
2020-12-19 23:08:01 -0400

    Falto el codigo fuente

---
+++ Proyecto de Censo/censado 2.0.cs
@@ -0,0 +1,129 @@
+using System;
+using System.IO;
+using System.Text;
+
+namespace censado 2.0
+{
+   public class main_class
+   {
+      static System.Random random_generator = new System.Random();
+      public static void Main(string[] args)
+      {
+         string raptor_prompt_variable_zzyz;
+         ?? a;
+         ?? p3;
+         ?? a2;
+         ?? a5;
+         ?? p5;
+         ?? p4;
+         ?? x;
+         ?? a3;
+         ?? p;
+         ?? p2;
+         ?? sexualidad;
+         ?? a4;
+         ?? volver;
+         ?? result;
+      
+         raptor_prompt_variable_zzyz ="Digite el nombre de la persona";
+         Console.WriteLine(raptor_prompt_variable_zzyz);
+         p= Double.Parse(Console.ReadLine());
+         raptor_prompt_variable_zzyz ="Digite la edad de la persona";
+         Console.WriteLine(raptor_prompt_variable_zzyz);
+         a= Double.Parse(Console.ReadLine());
+         raptor_prompt_variable_zzyz ="Mencione el sexo";
+         Console.WriteLine(raptor_prompt_variable_zzyz);
+         sexualidad= Double.Parse(Console.ReadLine());
+         raptor_prompt_variable_zzyz ="Desea agregar a otra persona (1.Si o 2. No)";
+         Console.WriteLine(raptor_prompt_variable_zzyz);
+         x= Double.Parse(Console.ReadLine());
+         if (x==1)
+         {
+            raptor_prompt_variable_zzyz ="Digite el nombre de la persona";
+            Console.WriteLine(raptor_prompt_variable_zzyz);
+            p2= Double.Parse(Console.ReadLine());
+            raptor_prompt_variable_zzyz ="Digite la edad de la persona";
+            Console.WriteLine(raptor_prompt_variable_zzyz);
+            a2= Double.Parse(Console.ReadLine());
+            raptor_prompt_variable_zzyz ="Mencione el sexo";
+            Console.WriteLine(raptor_prompt_variable_zzyz);
+            sexualidad= Double.Parse(Console.ReadLine());
+            raptor_prompt_variable_zzyz ="Desea agregar a otra persona (1.Si o 2. No)";
+            Console.WriteLine(raptor_prompt_variable_zzyz);
+            x= Double.Parse(Console.ReadLine());
+            if (x==1)
+            {
+               raptor_prompt_variable_zzyz ="Digite el nombre de la persona";
+               Console.WriteLine(raptor_prompt_variable_zzyz);
+               p3= Double.Parse(Console.ReadLine());
+               raptor_prompt_variable_zzyz ="Mencione el sexo";
+               Console.WriteLine(raptor_prompt_variable_zzyz);
+               sexualidad= Double.Parse(Console.ReadLine());
+               raptor_prompt_variable_zzyz ="Digite la edad de la persona";
+               Console.WriteLine(raptor_prompt_variable_zzyz);
+               a3= Double.Parse(Console.ReadLine());
+               raptor_prompt_variable_zzyz ="Desea agregar a otra persona (1.Si o 2. No)";
+               Console.WriteLine(raptor_prompt_variable_zzyz);
+               x= Double.Parse(Console.ReadLine());
+               if (x==1)
+               {
+                  while (1)
+                  {
+                     raptor_prompt_variable_zzyz ="Digite el nombre de la persona";
+                     Console.WriteLine(raptor_prompt_variable_zzyz);
+                     p4= Double.Parse(Console.ReadLine());
+                     raptor_prompt_variable_zzyz ="Digite la edad de la persona";
+                     Console.WriteLine(raptor_prompt_variable_zzyz);
+                     a4= Double.Parse(Console.ReadLine());
+                     raptor_prompt_variable_zzyz ="Mencione el sexo";
+                     Console.WriteLine(raptor_prompt_variable_zzyz);
+                     sexualidad= Double.Parse(Console.ReadLine());
+                     raptor_prompt_variable_zzyz ="Desea agregar a otra persona (1.Si o 2. No)";
+                     Console.WriteLine(raptor_prompt_variable_zzyz);
+                     x= Double.Parse(Console.ReadLine());
+                     raptor_prompt_variable_zzyz ="Esta aplicacion aun se encuentra en fase beta por lo que el censado esta limitado a 4 personas (Si desea volver a asignar la cuarta persona escriba volver- Si desea obtener resultados escriba ok)";
+                     Console.WriteLine(raptor_prompt_variable_zzyz);
+                     result= Double.Parse(Console.ReadLine());
+                     if (result==volver)) break;
+                  }
+                  raptor_prompt_variable_zzyz ="Digite el nombre de la persona";
+                  Console.WriteLine(raptor_prompt_variable_zzyz);
+                  p5= Double.Parse(Console.ReadLine());
+                  raptor_prompt_variable_zzyz ="Digite la edad de la persona";
+                  Console.WriteLine(raptor_prompt_variable_zzyz);
+                  a5= Double.Parse(Console.ReadLine());
+                  Console.WriteLine("Las personas que viven en esta vivienda son :"+p);
+                  Console.WriteLine("Con una edad de..."+a);
+                  Console.WriteLine("La siguiente es  :"+p2);
+                  Console.WriteLine("Con una edad de..."+a2);
+                  Console.WriteLine("La siguiente es  :"+p3);
+                  Console.WriteLine("Con una edad de..."+a3);
+                  Console.WriteLine("La siguiente es  :"+p4);
+                  Console.WriteLine("Con una edad de..."+a4);
+               }
+               else
+               {
+                  Console.WriteLine("Las personas que viven en esta vivienda son :"+p);
+                  Console.WriteLine("Con una edad de..."+a);
+                  Console.WriteLine("La siguiente es  :"+p2);
+                  Console.WriteLine("Con una edad de..."+a2);
+                  Console.WriteLine("La siguiente es  :"+p3);
+                  Console.WriteLine("Con una edad de..."+a3);
+               }
+            }
+            else
+            {
+               Console.WriteLine("Las personas que viven en esta vivienda son :"+p);
+               Console.WriteLine("Con una edad de..."+a);
+               Console.WriteLine("La siguiente es  :"+p2);
+               Console.WriteLine("Con una edad de..."+a2);
+            }
+         }
+         else
+         {
+            Console.WriteLine("Las personas que viven en esta vivienda son :"+p);
+            Console.WriteLine("Con una edad de..."+a);
+         }
+      }
+   }
+}
